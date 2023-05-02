384b7ce45523068ee860e46854fd93037fdac9bb
Cianuro-83 <buzzeosimone@yahoo.com>
2023-05-02 16:18:38 +0200

    FEAT: Api creata, testata e funzionante

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,12 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
 
+
 class ProjectController extends Controller
 {
     public function index()
     {
 
-        $results = Project::with('type:id,name', 'type.projects', 'technologies', 'user')->paginate(5);
+        $results = Project::with('type:id,name', 'type.projects', 'technologies')->paginate(5);
 
         return response()->json([
             'success' => true,

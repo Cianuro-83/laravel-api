624d7e586f18065530c4acb63d0d4635380c3e1f
Cianuro-83 <buzzeosimone@yahoo.com>
2023-05-02 15:39:36 +0200

    Try to create api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+
+        $results = Project::with('type:id,name', 'type.projects', 'technologies', 'user')->paginate(5);
+
+        return response()->json([
+            'success' => true,
+            'results' => $results,
+        ]);
+    }
+}
